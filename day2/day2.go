7e6a5c1832124981f10a7040bd645dfadd078620
Calum McVeigh <calum.mcveigh@opencastsoftware.com>
2025-12-11 11:01:49 +0000

    feat: day 2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -54,12 +54,14 @@ func GetIdRange(idRange string) IdRange {
 
 func IsInvalidId(id int) bool {
 	idStr := strconv.Itoa(id)
-	mid := len(idStr) / 2
+	idStrLen := len(idStr)
+
+	mid := idStrLen / 2
 	for i := 1; i < mid+1; i++ {
 		seqVal := idStr[0:i]
 		seqInstancesLen := len(strings.Split(idStr, seqVal)) - 1
 
-		if seqInstancesLen*i == len(idStr) {
+		if seqInstancesLen*i == idStrLen {
 			return true
 		}
 	}
